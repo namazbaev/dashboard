c0c16a243d9c25297856375bb422e10218b6ac5d
shakxzod_namazbaev <shaxkzod.namazbaev.9700@gmail.com>
2021-12-05 13:22:29 +0500

    alert

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-let main = document.querySelector(".main");
-let toggle = document.querySelector(".toggle");
-let navigation = document.querySelector(".navigation");
-let list = document.querySelectorAll(".navigation li");
+const main = document.querySelector(".main");
+const viewAll = document.querySelector(".btn");
+const toggle = document.querySelector(".toggle");
+const navigation = document.querySelector(".navigation");
+const list = document.querySelectorAll(".navigation li");
 
+viewAll.onclick = function() {
+  alert('All orders!')
+}
 toggle.onclick = function(){
     main.classList.toggle('active');
     navigation.classList.toggle('active');
 }
-
 function activeLink() {
   list.forEach((item) => item.classList.remove("hovered"));
   this.classList.add("hovered");
